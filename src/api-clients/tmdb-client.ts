edc8e4218630196cc0c99307ab9faf642006610d
sungyong <sungyong@gmail.com>
2021-06-09 13:25:47 +0900

    Add mirage mock server

--- src/api-clients/tmdb-client.ts
+++ src/api-clients/tmdb-client.ts
@@ -2,7 +2,8 @@ import { sleepAsync } from '../utils'
 
 // put your own tmdb access token here to make the app work.
 // you can register and get one here: https://www.themoviedb.org/signup
-const accessToken = 'Add your access token'
+const accessToken =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTMxNGRhMzMzMTgzNDI1Zjk2ZmVjYzAzNDJiMDI3NSIsInN1YiI6IjYwYmYxMDk2MGJjNTI5MDA2ZjI5YjYwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u6HxKbJHwg8kBvsURIHePrJpKGJXCKV9V8onBXJTWTE'
 
 const baseUrl = 'https://api.themoviedb.org/3'
 const authHeader = {
