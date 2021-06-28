dbdd539979d40dee3e18fa4429f6f5e778709d39
sungyong <sungyong@gmail.com>
2021-06-29 03:47:35 +0900

    Upgrade Vuex

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import createMockServer from '../mock-server/index'
+
+if (process.env.NODE_ENV === 'development') {
+  createMockServer()
+  console.log('API Mock Server started')
+}
 
 export default defineComponent({
   name: 'App',
